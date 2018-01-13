3762c0883006cfac8a42da8f51c27c68c6d06a87
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 21:19:35 -0800

    create PostsNew component

---
+++ src/components/posts_new.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PostsNew extends Component {
+    render() {
+        return (
+            <div>
+                PostsNew
+            </div>
+        );
+    }
+}

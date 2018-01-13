3aa05976b4a07440fe412f874e6b8344b1401fa2
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 21:40:12 -0800

    fixed bug in Router by using "Switch"

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import './index.css';
@@ -15,12 +15,15 @@ import registerServiceWorker from './registerServiceWorker';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
+// Inside "Switch", put the most special path on the top
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
         <BrowserRouter>
             <div>
-                <Route path="/" component={PostsIndex} />
-                <Route path="/posts/new" component={PostsNew} />
+                <Switch>
+                    <Route path="/posts/new" component={PostsNew} />
+                    <Route path="/" component={PostsIndex} />
+                </Switch>
             </div>
         </BrowserRouter>
     </Provider>,

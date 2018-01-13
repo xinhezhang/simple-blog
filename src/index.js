01f7dbb5f33c23f2c54bbc09968e7fa23b931ac7
xinhezhang <xinhezhang@hotmail.com>
2018-01-13 00:07:38 -0800

    add PostsShow to address path

--- src/index.js
+++ src/index.js
@@ -10,18 +10,21 @@ import App from './components/App';
 import reducers from "./reducers";
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 import registerServiceWorker from './registerServiceWorker';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
-// Inside "Switch", put the most special path on the top
+// Inside "Switch", put the most specific path on the top
+// ':id' is the wildcard
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
         <BrowserRouter>
             <div>
                 <Switch>
                     <Route path="/posts/new" component={PostsNew} />
+                    <Route path="/posts/:id" component={PostsShow} />
                     <Route path="/" component={PostsIndex} />
                 </Switch>
             </div>

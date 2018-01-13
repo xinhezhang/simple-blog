2349a1ef73b95886a5600a0aadacbd1c260c5666
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 18:03:06 -0800

    clean test code

--- src/index.js
+++ src/index.js
@@ -7,29 +7,17 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import reducers from "./reducers";
+import PostsIndex from './components/posts_index';
+
 import registerServiceWorker from './registerServiceWorker';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-    render() {
-        return <div>Hello!</div>;
-    }
-}
-
-class GoodBye extends React.Component {
-    render() {
-        return <div>GoodBye!</div>;
-    }
-}
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
         <BrowserRouter>
             <div>
-                Header
-                <Route path="/hello" component={Hello} />
-                <Route path="/goodbye" component={GoodBye} />
+                <Route path="/" component={PostsIndex} />
             </div>
         </BrowserRouter>
     </Provider>,

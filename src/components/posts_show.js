1d1c11960b3486468bd12a7731c009604337072b
xinhezhang <xinhezhang@hotmail.com>
2018-01-13 00:41:19 -0800

    add Back button ('Link') to post detail page

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { fetchPost } from '../actions';
 
@@ -22,6 +23,7 @@ class PostsShow extends Component {
 
         return (
             <div>
+                <Link to="/" className="btn btn-primary">Back</Link>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>

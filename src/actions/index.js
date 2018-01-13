ca5f4c3c5936fd40e4bd188f5218ddb226204172
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 23:45:11 -0800

    add action creator for create post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 // api doc: http://reduxblog.herokuapp.com/
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -11,6 +12,16 @@ export function fetchPosts() {
 
     return {
         type: FETCH_POSTS,
+        payload: request,
+    };
+}
+
+// action creator for create post
+export function createPost(values) {
+    const request = axios.post(`${ROOT_URL}/posts?key=${API_KEY}`, values);
+
+    return {
+        type: CREATE_POST,
         payload: request,
     };
 }

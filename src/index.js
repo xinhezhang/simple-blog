c376c7448ac47ffcf07fd043e68b7709fa02c350
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 17:32:47 -0800

    fixed bug by update "react-router-dom" and use "npm run start"

--- src/index.js
+++ src/index.js
@@ -2,18 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import promise from "redux-promise";
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import './index.css';
 import App from './components/App';
 import reducers from "./reducers";
 import registerServiceWorker from './registerServiceWorker';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends React.Component {
+    render() {
+        return <div>Hello!</div>;
+    }
+}
+
+class GoodBye extends React.Component {
+    render() {
+        return <div>GoodBye!</div>;
+    }
+}
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-        <App />
+        <BrowserRouter>
+            <div>
+                <Route path="/hello" component={Hello} />
+                <Route path="/goodbye" component={GoodBye} />
+            </div>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 );

b9f91d8a9abfa7ad63b02973ebc71e9169cf29a0
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 18:47:38 -0800

    import "promise" and add to "applyMiddleware"

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
 import './index.css';
 import App from './components/App';
@@ -11,7 +12,7 @@ import PostsIndex from './components/posts_index';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>

01eab33929bddfa3d375f4a3df2e8d5aba373c4d
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 17:12:14 -0800

    install redux-promise and applyMiddleware

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import promise from "redux-promise";
+
 import './index.css';
 import App from './components/App';
 import reducers from "./reducers";
 import registerServiceWorker from './registerServiceWorker';
 
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={createStoreWithMiddleware(reducers)}>
         <App />
     </Provider>,
     document.getElementById('root')

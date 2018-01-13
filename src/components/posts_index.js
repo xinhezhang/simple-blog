1da974aeb229e66a2d5ad30dbc1ed484a0d538a6
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 21:47:04 -0800

    add navigation between Index and New

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';    // like "anchor tag" in HTML
 import _ from 'lodash';
 
 import { fetchPosts } from '../actions/index';

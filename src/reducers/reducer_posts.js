4bc7776a0e06bbb51e70f07c28d906fafc76a1b6
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 19:10:45 -0800

    implement index action (use post's id as key to the array posts)

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,11 @@
 import { FETCH_POSTS } from '../actions/index';
+import _ from 'lodash';
 
 export default function (state = {}, action) {
     switch (action.type) {
         case FETCH_POSTS:
-            console.log(action.payload.data);   // [ post1, post2, ... ]
-            break;
+            //console.log(action.payload.data);   // [ post1, post2, ... ]
+            return _.mapKeys(action.payload.data, 'id');    // posts['id']
         default:
             return state;
     }

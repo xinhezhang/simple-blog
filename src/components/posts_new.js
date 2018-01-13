ac44e519c511224c635a5ee791add756e4eedcbd
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 22:24:14 -0800

    implement reduxForm for PostsNew component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';  // 'reduxForm' is similar to 'connect' in react-redux
 
-export default class PostsNew extends Component {
-    render() {
+class PostsNew extends Component {
+    // we need 'field' argument since we may need to handle different kinds of components
+    renderTitleField(field) {
         return (
             <div>
-                PostsNew
+                <input
+                    type="text"         // text input
+                    {...field.input}    // field.input -> objects (including event-handlers, props)
+                />
             </div>
         );
     }
+
+    render() {
+        return (
+            <form action="">
+                <Field          // Field is responsible for event-handling and updating different piece of states
+                    name="title"
+                    component={this.renderTitleField}   // this is a function reference, not a function call
+                />
+            </form>
+        );
+    }
 }
+
+// form property MUST be unique, so redux-form could handle multiple forms in same page at the same time
+export default reduxForm({
+    form: 'PostsNewForm',
+})(PostsNew);

7e485098826e3e2c6b909200bd957b9072c7f56c
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 20:03:39 -0800

    implement renderPosts() using lodash map function (work for objects)

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
+import _ from 'lodash';
 
 class PostsIndex extends Component {
     // fetching data after the component been rendered,
@@ -10,12 +11,25 @@ class PostsIndex extends Component {
         this.props.fetchPosts();
     }
 
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return (
+                <li className="list-group-item" key={post.id} >
+                    {post.title}
+                </li>
+            );
+        });
+    }
+
     render() {
-        console.log(this.props.posts);
-        
+        //console.log(this.props.posts);
+
         return (
             <div>
-                Posts Index
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
